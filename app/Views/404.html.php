3396c5fecd8478f6317ef5b29dd580dae0996f92
RomaVan <lasedwe@gmail.com>
2017-02-21 16:20:47 +0200

    add warning and error 404

---
+++ app/Views/404.html.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="text-center">
+        <h1>ERROR 404</h1>
+    </div>
+    <hr>
+    <div class="alert alert-danger text-center">
+        <strong>Warning!</strong> Page not found
+    </div>
+</div>
